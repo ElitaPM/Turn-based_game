ca0311934ae201fff6afc4fe9e79faed465fbecc
Zhenochek <rogow.zhen@yandex.ru>
2018-04-21 15:29:43 +0300

    ss

--- field.h
+++ field.h
@@ -4,6 +4,8 @@
 #include "barrier.h"
 #include "soldier.h"
 #include "warbase.h"
+#include <typeinfo>
+#include <vector>
 
 class Field
 {
@@ -11,7 +13,17 @@ private:
     Unit *ptr[20][20];
 public:
     Field();
-    void move(Soldier &soldier);
+    void move(Unit *soldier);
+    void units_simbol(std::vector<std::vector<char> > &vec);
+    void init(Unit* yellowUnit_1,
+              Unit* yellowUnit_2,
+              Unit* yellowUnit_3,
+              Unit* blueUnit_1,
+              Unit* blueUnit_2,
+              Unit* blueUnit_3,
+              WarBase& blueWarBase,
+              WarBase& yellowWarBase,
+              Barrier& barrier);
 };
 
 #endif // FIELD_H
