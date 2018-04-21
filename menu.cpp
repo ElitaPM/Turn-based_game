35097e9db3898c32da342ea8697bad5f9a46ba87
Zhenya <rogow.zhen@yandex.ru>
2018-04-21 14:54:01 +0300

    dd

--- menu.cpp
+++ menu.cpp
@@ -1,7 +1,68 @@
 #include "menu.h"
-/*
+
 Menu::Menu()
 {
-    print();
+    for (int j = 0; j <= 20; j++)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            field[2*j][i*4] = '+';
+            field[2*j][i*4+1] = '-';
+            field[2*j][i*4+2] = '-';
+            field[2*j][i*4+3] = '-';
+        }
+        field [2*j][80] = '+';
+        field [2*j][81] = '\n';
+
+        if (j != 20)
+        {
+            for (int k = 0; k < 20; k++)
+            {
+                field[2*j+1][k*4] = '|';
+                field[2*j+1][k*4+1] = ' ';
+                field[2*j+1][k*4+2] = ' ';
+                field[2*j+1][k*4+3] = ' ';
+            }
+            field [2*j+1][80] = '|';
+            field [2*j+1][81] = '\n';
+        }
+
+    }
 }
-*/
+
+void Menu::print(Field &field)
+{
+    std::vector<std::vector<char>> bukafki;
+    bukafki.reserve(20);
+            field.units_simbol(bukafki);
+
+
+    for (int j = 0; j < 20; j++)
+    {
+        for (int i= 0; i < 20; i++)
+        {
+            std::cout << (bukafki[j])[i] << " ";
+            if ((bukafki[j])[i] == 'Б' || (bukafki[j])[i] == 'О')
+            {
+                this->field[1+2*j][2+4*i-1] = '|';
+                this->field[1+2*j][2+4*i] = '|';
+                this->field[1+2*j][2+4*i+1] = '|';
+            }
+            else this->field[1+2*j][2+4*i] = (bukafki[j])[i];
+
+        }
+        std::cout << std::endl;
+    }
+
+}
+
+void Menu::print()
+{
+    for (int j=0; j<41; j++)
+    {
+        for (int i=0; i<82; i++)
+        {
+            std::cout << this->field[j][i];
+        }
+    }
+}
--- menu.h
+++ menu.h
@@ -1,13 +1,16 @@
 #ifndef MENU_H
 #define MENU_H
 #include "field.h"
+#include <iostream>
 
 class Menu
 {
 private:
+    char field [41][82];
 public:
     Menu();
     void print(Field &field);
+    void print();
 };
 
 #endif // MENU_H
